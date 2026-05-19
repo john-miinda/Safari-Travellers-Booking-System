9d59ca0eef873bfbae866503f096c750436e8a71
John Miinda <miindagijo@gmail.com>
2026-05-19 12:15:16 +0300

    security: Remove hardcoded database credentials and SECRET_KEY from config

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get(
-        'SECRET_KEY',
-        '4eac137d3bc799d3a30eb5d84dea8bdcc7c33aaef085cc8339c7a2e9fd7ed12e'
-    )
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        'postgresql://neondb_owner:npg_W7NgTZ4wdyjo@ep-blue-cell-apzu47ml-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
-    )
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    if not SECRET_KEY:
+        raise ValueError('SECRET_KEY environment variable is not set')
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if not SQLALCHEMY_DATABASE_URI:
+        raise ValueError('DATABASE_URL environment variable is not set')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
@@ -22,12 +22,9 @@ class Config:
     ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
 
-    
     SQLALCHEMY_ENGINE_OPTIONS = {
         'pool_pre_ping': True,
         'pool_size': 5,
         'max_overflow': 10,
         'pool_timeout': 30,
     }
-
-    
